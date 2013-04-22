c35bc4167a9337adf197a8cc8b92b5c843e5fc15
Jason Novek <jnovek@euclid.gateway.2wire.net>
2013-04-21 21:32:59 -0500

    First try at including an indicator of whether you're ahead or behind origin.

--- branchulator.rb
+++ branchulator.rb
@@ -13,12 +13,26 @@ def merge_conflict?
 end
 
 def origin_status(branch)
-  ""
+  # First make sure that there's a remote branch
+  return "" if `git rev-list origin/master..HEAD 2>&1` =~ /fatal/
+  # Check for remote changes
+  remote_changes = `git rev-list HEAD..origin/master`.chomp.split("\n").count
+  return "[-#{remote_changes}]" if remote_changes > 0
+  # Check for local changes
+  local_changes = `git rev-list origin/master..HEAD`.chomp.split("\n").count
+  return "[+#{local_changes}]" if local_changes > 0
+  # OK, looks like we're square!
+  "[0]"
+end
+
+def fatal?
+  (`git rev-parse --git-dir 2>&1` =~ /fatal/) != nil ||
+  (`git rev-parse --abbrev-ref HEAD 2>&1` =~ /fatal/) != nil
 end
 
 # Program body
 # Don't do this if we're not in a working repo
-unless `git rev-parse --git-dir 2>&1` =~ /fatal/i
+unless fatal?
 
   branch = `git rev-parse --abbrev-ref HEAD 2>&1`.chomp
 
